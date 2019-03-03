8238210f12dbb4d1864c279e6f5dfdd3a0b44286
sptea <00ing.n@gmail.com>
2019-03-03 20:41:39 +0900

    [update] Updated sqlite-migrate

--- dev-db.sh
+++ dev-db.sh
@@ -1,6 +1,4 @@
-echo -e '\n####### Remove Database #######\n'
-rm database-dev.db
-echo -e '\n####### Create Table #######\n'
+echo -e '\n\n####### Create Table #######\n'
 go run sql/sqlite-migrate/main.go database-dev.db sql/migration
-echo -e '\n####### Insert test data #######\n'
+echo -e '\n\n####### Insert test data #######\n'
 go run sql/sqlite-migrate/main.go database-dev.db sql/test-data
